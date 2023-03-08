d8db3ac3b2aa756616c4d516427d1911acde7f0d
kphutiyagae <kopanophutiyagae@gmail.com>
2023-03-09 00:40:27 +0200

    feat: :construction: Added apiRoutes and api.tsx

---
+++ src/api/apiRoutes.tsx
@@ -0,0 +1,11 @@
+const baseUrl = 'https://api.mangadex.org';
+
+const mangaUrl = `${baseUrl}/manga`;
+
+const userUrl = `${baseUrl}/user`;
+
+const ratingUrl = `${baseUrl}/rating`;
+
+const statisticsUrl = `${baseUrl}/statistics`;
+
+export { mangaUrl, userUrl, ratingUrl, statisticsUrl };
